112ca4e64a8d7864b750962bbf6df8211c5d973c
Shafayathub <shafayat.ms@gamilcom>
2022-06-28 22:25:16 +0600

    release date issue fixed

--- js/phone.js
+++ js/phone.js
@@ -1,28 +1,41 @@
 const searchPhone = () => {
-  const searchText = document.getElementById('search-field').value;
-  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((phoneData) => getData(phoneData.data));
+  const searchField = document.getElementById('search-field');
+  const searchText = searchField.value;
+  document.getElementById('spinner').style.display = 'block';
+  // clear search box
+  searchField.value = '';
+  if (searchText == '') {
+    alert('Please enter text to search');
+  } else {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((phoneData) => getData(phoneData.data));
+  }
 };
+
 const getData = (phoneData) => {
   phoneData.forEach((data) => {
-    releaseDate(data);
+    slugData(data);
   });
 };
-const releaseDate = (data) => {
+const slugData = (data) => {
   const id = data.slug;
   fetch(` https://openapi.programming-hero.com/api/phone/${id}`)
     .then((res) => res.json())
     .then((slugData) => showData(slugData.data));
 };
 const showData = (data) => {
+  const preSearchResult = document.getElementById('pre-search-result');
+  // clear loaded content
+  preSearchResult.textContent = '';
   const searchResult = document.getElementById('search-result');
   const div = document.createElement('div');
   div.classList.add('col');
-  div.innerHTML = `
+  if (data.releaseDate != '') {
+    div.innerHTML = `
             <div class="card h-100">
-                <img src="${data.image}" class="card-img-top">
+                <img src="${data.image}">
                 <div class="card-body">
                     <h5 class="card-title">${data.brand}</h5>
                     <h5 class="card-title">${data.name}</h5>
@@ -30,10 +43,30 @@ const showData = (data) => {
                 </div>
                 <div class="card-footer">
                    <small class="text-muted">
-                      <a target=_blank href="#" class="btn btn-warning stretched-link">Details</a>
+                      <a target=_blank href="#" class="btn btn-info stretched-link">Details</a>
                    </small>
                 </div>
             </div>
       `;
-  searchResult.appendChild(div);
+    searchResult.appendChild(div);
+  } else {
+    div.innerHTML = `
+            <div class="card h-100">
+                <img src="${data.image}">
+                <div class="card-body">
+                    <h5 class="card-title">${data.brand}</h5>
+                    <h5 class="card-title">${data.name}</h5>
+                    <p class="card-text">${'not found'}</p>
+                </div>
+                <div class="card-footer">
+                   <small class="text-muted">
+                      <a target=_blank href="#" class="btn btn-info stretched-link">Details</a>
+                   </small>
+                </div>
+            </div>
+      `;
+    searchResult.appendChild(div);
+  }
+
+  document.getElementById('spinner').style.display = 'none';
 };
