5f4ab3b11e950db673de2ebe91ae44cfccd02851
Shafayathub <shafayat.ms@gamilcom>
2022-06-29 02:15:47 +0600

    image size and alignment fixed

--- js/phone.js
+++ js/phone.js
@@ -4,6 +4,9 @@ const searchPhone = () => {
   document.getElementById('spinner').style.display = 'block';
   // clear search box
   searchField.value = '';
+  const searchResult = document.getElementById('search-result');
+  // clear loaded content
+  searchResult.textContent = '';
   if (searchText == '') {
     alert('Please enter text to search');
   } else {
@@ -15,9 +18,38 @@ const searchPhone = () => {
 };
 
 const getData = (phoneData) => {
-  phoneData.forEach((data) => {
-    slugData(data);
-  });
+  if (phoneData.length !== 0) {
+    // showing 20 results
+    newPhoneData = phoneData.slice(0, 20);
+    newPhoneData.forEach((data) => {
+      slugData(data);
+    });
+  } else {
+    const preSearchResult = document.getElementById('pre-search-result');
+    // clear loaded content
+    preSearchResult.textContent = '';
+    const div = document.createElement('div');
+    div.innerHTML = `<div id="pre-search-result" class="container">
+        <div class="card text-center">
+            <div class="card-body">
+                <blockquote class="blockquote mb-0">
+                    <p>No phone found. Choose better search another.</p>
+                    <footer class="blockquote-footer">Another learner <cite title="Source Title">MD shafayat
+                            Islam</cite>
+                    </footer>
+                </blockquote>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">
+                    <a target=_blank href="https://sites.google.com/view/shafayat/home"
+                        class="btn btn-info stretched-link">Meet the developer</a>
+                </small>
+            </div>
+        </div>
+    </div>`;
+    preSearchResult.appendChild(div);
+    document.getElementById('spinner').style.display = 'none';
+  }
 };
 const slugData = (data) => {
   const id = data.slug;
@@ -35,7 +67,7 @@ const showData = (data) => {
   if (data.releaseDate != '') {
     div.innerHTML = `
             <div class="card h-100">
-                <img src="${data.image}">
+                <img src="${data.image}" class="card-img-top mx-auto" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${data.brand}</h5>
                     <h5 class="card-title">${data.name}</h5>
@@ -52,7 +84,7 @@ const showData = (data) => {
   } else {
     div.innerHTML = `
             <div class="card h-100">
-                <img src="${data.image}">
+                <img src="${data.image}" class="card-img-top mx-auto" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${data.brand}</h5>
                     <h5 class="card-title">${data.name}</h5>
