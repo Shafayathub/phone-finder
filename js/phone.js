e0da6e6b53fed862b58c9dbc820fbbbf8ae8dba6
Shafayathub <shafayat.ms@gamilcom>
2022-06-28 21:49:42 +0600

    release date added

--- js/phone.js
+++ js/phone.js
@@ -3,25 +3,37 @@ const searchPhone = () => {
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayResult(data.data));
+    .then((phoneData) => getData(phoneData.data));
 };
-const displayResult = (phones) => {
+const getData = (phoneData) => {
+  phoneData.forEach((data) => {
+    releaseDate(data);
+  });
+};
+const releaseDate = (data) => {
+  const id = data.slug;
+  fetch(` https://openapi.programming-hero.com/api/phone/${id}`)
+    .then((res) => res.json())
+    .then((slugData) => showData(slugData.data));
+};
+const showData = (data) => {
   const searchResult = document.getElementById('search-result');
-  phones.forEach((data) => {
-    const div = document.createElement('div');
-    div.classList.add('col');
-    div.innerHTML = `<div class="card">
-                <img width=100% src="${data.image}" class="card-img-top">
+  const div = document.createElement('div');
+  div.classList.add('col');
+  div.innerHTML = `
+            <div class="card h-100">
+                <img src="${data.image}" class="card-img-top">
                 <div class="card-body">
-                    <h5 class="card-title">${data.phone_name}</h5>
                     <h5 class="card-title">${data.brand}</h5>
+                    <h5 class="card-title">${data.name}</h5>
+                    <p class="card-text">${data.releaseDate}</p>
                 </div>
                 <div class="card-footer">
                    <small class="text-muted">
                       <a target=_blank href="#" class="btn btn-warning stretched-link">Details</a>
                    </small>
                 </div>
-            </div>`;
-    searchResult.appendChild(div);
-  });
+            </div>
+      `;
+  searchResult.appendChild(div);
 };
